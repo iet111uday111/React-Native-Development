8718e4540ea46ce3235e22c034c25d096d5807f5
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-09 12:04:53 +0530

    "Test for emulator"

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>UdAYADITYA sINGH</Text>
+        <Text style={styles.welcome}>Hello World</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
