fc491193e1d32f04853b5fae64d8b8a59e2a75f6
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-09 19:40:08 +0530

    "Header CSS Adjacement"

--- AwesomeProjects/src/components/header.js
+++ AwesomeProjects/src/components/header.js
@@ -21,20 +21,19 @@ const Header = () => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#e5d7d7',
+        backgroundColor: '#F8F8F8',
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
         paddingTop: 15,
         shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
+        shadowOffset: { width: 2, height: 10 },
+        shadowOpacity: .7,
         elevation: 2,
         position: 'relative'
     },
     textStyle: {
-        fontSize: 20,
-        color: '#ba6a6a'
+        fontSize: 28
     }
 };
 
