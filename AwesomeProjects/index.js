bf25dac88c43c5dbef306856c973091f658ef020
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-09 16:50:30 +0530

    "Creating a basic Component"

--- AwesomeProjects/index.js
+++ AwesomeProjects/index.js
@@ -1,3 +1,22 @@
+/*
+*  Import Libraries to help create a Component
+ */
 import React from 'react';
 import ReactNative from 'react-native';
 
+/*
+* Create a Component
+*/
+const App = () => {
+    return (
+        <Text>
+            Udayaditya Singh
+        </Text>
+    );
+}
+
+/*
+* Render it to Device
+*/
+
+ReactNative.AppRegistry.registerComponent('albums', () => App)
