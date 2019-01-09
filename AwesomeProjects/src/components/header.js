f0ac760b73e5588e84e2475ee7a59175fd58e4e3
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-09 18:16:45 +0530

    "Formatted header.js"

--- AwesomeProjects/src/components/header.js
+++ AwesomeProjects/src/components/header.js
@@ -2,16 +2,16 @@
 * Import libraries for making a component
  */
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 /*
 * Make a component
 */
 const Header = () => {
-    const {viewStyle, textStyle} = styles;
+    const { viewStyle, textStyle } = styles;
 
     return (
-        <View style = {viewStyle}>
+        <View style={viewStyle}>
             <Text>
                 <Text style={textStyle}>Albums</Text>
             </Text>
@@ -20,21 +20,21 @@ const Header = () => {
 }
 
 const styles = {
-  viewStyle: {
-    backgroundColor: '#F8F8F8',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 60,
-    paddingTop: 15,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: 'relative'
-  },
-  textStyle: {
-    fontSize: 20
-  }
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
 };
 
 
