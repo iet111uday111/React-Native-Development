cac92ec77b2b796c9aef8f84976ba14a56c35349
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-10 16:10:25 +0530

    "Working on JSX of Album Detail"

--- AwesomeProjects/src/components/AlbumDetail.js
+++ AwesomeProjects/src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Text } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -8,10 +8,24 @@ const AlbumDetail = (props) => {
     return (
         <Card>
             <CardSection>
+                <View>
+                      
+                </View>
+                <View style={styles.headerContentStyles}>
+                     <Text>{props.album.title}</Text>
+                     <Text>{props.album.artist}</Text>
+                </View>
                 <Text>{props.album.title}</Text>
             </CardSection>
         </Card>
     );
+}
+
+const styles = {
+    headerContentStyles: {
+        flexDirection:'column',
+        justifyConent:'space-around'
+    }
 }
 
 export default AlbumDetail;
