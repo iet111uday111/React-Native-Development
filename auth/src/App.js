fc4e2c6f850feaf54101ac24fec70ff60ac92e80
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 15:02:54 +0530

    "Login form Component"

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import firebase from 'firebase';
+import firebase from '@firebase/app';
 
 import { Header } from './components/common';
 import { config } from './config'
+import LoginForm from './components/LoginForm';
+
 export default class App extends Component {
   componentWillMount() {
-    firebase.initializeApp(config);
+    const fire = firebase.initializeApp(config);
+    console.log(fire);
+    
   }
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     )
   }
