bd24f23161ab1c2b942501061c995a56df9d44ef
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 13:39:47 +0530

    "Firebase Setup"

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
-import { Header } from './components/common';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
+import { Header } from './components/common';
+import { config } from './config'
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-        <View>
-            <Header headerText='Authentication'/>
-            <Text>An App</Text>
-        </View>
+      <View>
+        <Header headerText='Authentication' />
+        <Text>An App</Text>
+      </View>
     )
   }
 };
