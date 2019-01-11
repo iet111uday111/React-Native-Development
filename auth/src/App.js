210c214459b09e6d05b6027c11451a05c30c7da8
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 19:18:46 +0530

    "Rendering Content for  checking user is loggedIn or not"

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,31 +2,44 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import { Firebase } from './config';
-
-import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
   state = {
     loggedIn: false
   }
-  
-  componentWillMount(){
+
+  componentWillMount() {
     firebase.auth().onAuthStateChanged((user) => {
-      if(user){
-        this.setState({loggedIn: true})
-      }else{
-        this.setState({loggedIn: false})
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
       }
     })
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
