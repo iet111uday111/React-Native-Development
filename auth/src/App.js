15739b074430bfa4328355a43a336e2ffd0fded8
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 15:30:41 +0530

    "Adding state of in TextInput"

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import firebase from '@firebase/app';
 
 import { Header } from './components/common';
-import { config } from './config'
+import { Firebase } from './config'
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
-  componentWillMount() {
-    const fire = firebase.initializeApp(config);
-    console.log(fire);
-    
-  }
   render() {
     return (
       <View>
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 export default class LoginForm extends Component {
-  render() {
-    return (
-        <Card>
-            <CardSection />
-            <CardSection />
-            
-            <CardSection>
-                <Button>
-                    LogIn
+    state= {
+        text: ''
+    };
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <TextInput 
+                        style={{ height: 40, flex: 1 }} 
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text})}    
+                    />
+                </CardSection>
+                <CardSection />
+
+                <CardSection>
+                    <Button>
+                        LogIn
                 </Button>
-            </CardSection>
-            
-        </Card>
-    )
-  }
+                </CardSection>
+
+            </Card>
+        )
+    }
 }
