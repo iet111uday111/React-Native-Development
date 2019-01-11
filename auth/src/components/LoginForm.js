811cccf0c1d80c2d5de5a86b5145902d3abf99dd
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 19:04:40 +0530

    "Complete Login Screen"

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,78 +1,90 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
-export default class LoginForm extends Component {
-    state = {
-        email: '',
-        password: '',
-        error: ''
-    };
+class LoginForm extends Component {
+  state = { email: '', password: '', error: '', loading: false };
 
-    onButtonPress = () => {
-        const { email, password } = this.state;
-        console.log(email);
-        console.log(password);
+  onButtonPress() {
+    const { email, password } = this.state;
 
-        this.setState({
-            error: ''
-        });
+    this.setState({ error: '', loading: true });
 
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(() => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
 
-            })
-            .catch(() => {
-                firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .then(() => {
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed', loading: false });
+  }
 
-                    })
-                    .catch(() => {
-                        this.setState({ error: 'Authentication Field.' })
-                    })
-            })
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
     }
 
-    render() {
-        return (
-            <Card>
-                <CardSection>
-                    <Input
-                        placeholder="user@gmail.com"
-                        label="Email"
-                        value={this.state.email}
-                        onChangeText={email => this.setState({ email })}
-                    />
-                </CardSection>
-                <CardSection>
-                    <Input
-                        secureTextEntry
-                        placeholder="password"
-                        label="Password"
-                        value={this.state.password}
-                        onChangeText={password => this.setState({ password })}
-                    />
-                </CardSection>
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
+  }
 
-                <Text style={styles.errorTextStyle}>
-                    {this.state.error}
-                </Text>
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
 
-                <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        LogIn
-                    </Button>
-                </CardSection>
-            </Card>
-        )
-    }
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          {this.renderButton()}
+        </CardSection>
+      </Card>
+    );
+  }
 }
 
 const styles = {
-    errorTextStyle: {
-        fontSize: 20,
-        alignSelf: 'center',
-        color: 'red'
-    }
-}
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+export default LoginForm;
