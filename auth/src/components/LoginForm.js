0cc2533f7af39ee1d474f5181e302aa3ac5acc83
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 17:54:58 +0530

    "Indentation"

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -18,7 +18,7 @@ export default class LoginForm extends Component {
         this.setState({
             error: ''
         });
-        
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(() => {
 
