fb9b532ef8b5c32b9e53c996deda2718e06dabf0
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-11 19:11:35 +0530

    "Check user is loggedIn or not"

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
-import { Firebase } from './config'
+import { Firebase } from './config';
+
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+  state = {
+    loggedIn: false
+  }
+  
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.setState({loggedIn: true})
+      }else{
+        this.setState({loggedIn: false})
+      }
+    })
+  }
+
   render() {
     return (
       <View>
