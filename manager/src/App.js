6af56075a14b67c564dceb4a3902b8789725cc5e
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-14 11:48:01 +0530

    "Created Skelton of login form"

--- manager/src/App.js
+++ manager/src/App.js
@@ -4,17 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Firebase } from './config';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-            <View>
-                <Text>
-                    Hello!
-                </Text>
-            
-            </View>
+            <LoginForm />
       </Provider>
     )
   }
