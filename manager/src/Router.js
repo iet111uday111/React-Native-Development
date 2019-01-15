397a3ae4c34a203c350879439aee02a445be2e94
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-15 15:27:37 +0530

    "Added more 1 Scene"

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" titleStyle={styles.navigationBarTitleStyle}>
-                <Scene key="Login" component={LoginForm} title="Please Login" initial={true} />
+                <Scene key="Login" component={LoginForm} title="Please Login" initial/>
+                <Scene key="EmployeeList" component={EmployeeList} title="Employees"/>
             </Scene>
+            
         </Router>
     )
 }
